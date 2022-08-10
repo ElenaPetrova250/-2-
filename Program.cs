937826bf1166fb451c075ea134b0691a631fd562
Elena Petrova <elena.i.11@yandex.ru>
2022-08-10 17:56:51 +0400

    task10

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введи трёхзначное число: ");
+int threeNumber = int.Parse(Console.ReadLine());
+string stringNumber = Convert.ToString(threeNumber);
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
